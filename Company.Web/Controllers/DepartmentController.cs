d4f8f64cad3e4593de5812cf5eb3e14d60d65e05
Ibrahimelsayed60 <hemasayed600@gmail.com>
2024-09-22 17:55:14 -0700

    Part 07 Department Controller - Details (Action & View)

--- Company.Web/Controllers/DepartmentController.cs
+++ Company.Web/Controllers/DepartmentController.cs
@@ -49,5 +49,17 @@ namespace Company.Web.Controllers
             }
 
         }
+
+        public IActionResult Details(int? id)
+        {
+            var dept = _departmentService.GetById(id);
+
+            if (dept is null)
+            {
+                return NotFound();
+            }
+
+            return View(dept);
+        }
     }
 }
