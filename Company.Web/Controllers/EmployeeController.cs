94bdecf3f52ecfcf2a506a8ae1636338aa103fa5
Ibrahimelsayed60 <hemasayed600@gmail.com>
2024-09-26 14:32:51 -0700

    Part 05 Employee Create (Action & View) - ViewBag-ViewData-TempData

--- Company.Web/Controllers/EmployeeController.cs
+++ Company.Web/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using Company.Service.Interfaces;
+﻿using Company.Data.Models;
+using Company.Service.Interfaces;
+using Company.Service.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Web.Controllers
@@ -6,10 +8,12 @@ namespace Company.Web.Controllers
     public class EmployeeController : Controller
     {
         private readonly IEmployeeService _employeeService;
+        private readonly IDepartmentService _departmentService;
 
-        public EmployeeController(IEmployeeService employeeService) 
+        public EmployeeController(IEmployeeService employeeService, IDepartmentService departmentService) 
         {
             _employeeService = employeeService;
+            _departmentService = departmentService;
         }
 
         [HttpGet]   
@@ -18,6 +22,10 @@ namespace Company.Web.Controllers
             if(string.IsNullOrEmpty(searchInp)) 
             {
                 var emp = _employeeService.GetAll();
+                //ViewBag.Message = "This is message from ViewBag";
+                //ViewData["TxtMessage"] = "This is message from ViewData";
+                //TempData.Keep("TxtMessage2");
+                //TempData["TxtMessage2"] = "This is message from TempData";
                 return View(emp); 
             }
             else
@@ -26,6 +34,38 @@ namespace Company.Web.Controllers
                 return View(emp);
             }
 
+
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+           ViewBag.Departments = _departmentService.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _employeeService.Add(employee);
+                    //TempData["TxtMessage2"] = "This is message from TempData";
+                    return RedirectToAction(nameof(Index));
+                }
+
+                ModelState.AddModelError("EmployeeError", "ValidationErrors");
+
+                return View(employee);
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("DepartmentError", ex.Message);
+                return View(employee);
+            }
 
         }
     }
