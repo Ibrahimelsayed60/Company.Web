868e32f0e904d7e72aee578dcbfcd1f18008a318
Ibrahimelsayed60 <hemasayed600@gmail.com>
2024-10-07 10:33:31 -0700

    Part 01 Account Controller (SignUp)

---
+++ Company.Web/Models/SignUpViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Web.Models
+{
+    public class SignUpViewModel
+    {
+        [Required(ErrorMessage ="First name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Format")]
+        public string Email { get; set; }
+
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$", ErrorMessage ="Password must be atleast 8 Characters")]
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        [Compare(nameof(Password), ErrorMessage ="Confirm password does not match password") ]
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "Is Active is required")]
+        public bool IsActive { get; set; }
+    }
+}
